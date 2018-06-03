626bc04dc6ced7e64511251fa6142c936825f5ba
Joshua Kerber <jkerber14@comcast.net>
2018-06-03 06:43:43 -0400

    sim v1.0 complete

---
+++ packages/constants.py
@@ -0,0 +1,19 @@
+"""
+constants.py
+
+Author:         Josh Kerber
+Date:           1 June 2018
+Description:    Constant variables for simulation
+"""
+
+# speed of particles (meters per second)
+VELOCITY_CONST = 0.02
+
+# simulation constants
+NUM_PARTICLES = 100
+NUM_MILLISECONDS = 10000
+
+# corner of room dimensions
+MAX_DISTANCE_CORNER = 10
+MAX_DISTANCE_ROOM = 100
+DISTANCE_DIGITS = 4
---
+++ packages/graphics.py
@@ -0,0 +1,46 @@
+"""
+graphics.py
+
+Author:         Josh Kerber
+Date:           1 June 2018
+Description:    Custom graphics library for simulation
+"""
+
+import matplotlib.pyplot as pyplot
+from mpl_toolkits.mplot3d import Axes3D
+import numpy
+
+#set display of axis
+AXIS = Axes3D(pyplot.figure())
+AXIS.minorticks_on()
+AXIS.tick_params(axis='both', which='minor', length=5, width=2, labelsize=15)
+AXIS.tick_params(axis='both', which='major', length=8, width=2, labelsize=15)
+
+# set window title
+FIGURE = pyplot.gcf()
+FIGURE.canvas.set_window_title('Smoke In Corner of Room Simulation')
+
+def setPlot(coordsX, coordsY, coordsZ, title):
+    """plot the smoke in the room on a 3D graph"""
+    global AXIS
+
+    # clear previous data
+    pyplot.cla()
+
+    # set limits of axis
+    AXIS.set_xlim(0,100)
+    AXIS.set_ylim(0,100)
+    AXIS.set_zlim(0,100)
+
+    # plot data
+    x = numpy.array(coordsX)
+    y = numpy.array(coordsY)
+    z = numpy.array(coordsZ)
+    AXIS.plot(x, y, z, 'ok')
+
+    # set title and pause graphics
+    pyplot.title(title)
+    pyplot.pause(0.05)
+
+def showPlot():
+    pyplot.show()
